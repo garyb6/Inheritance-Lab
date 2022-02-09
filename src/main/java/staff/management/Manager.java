f958c99d2cd3dc770e75c9fd4f5f8c718dbd95a4
garyb6 <gary.bennett6@outlook.com>
2022-02-09 14:21:28 +0000

    getDeptName test passed

--- src/main/java/staff/management/Manager.java
+++ src/main/java/staff/management/Manager.java
@@ -1,4 +1,18 @@
 package staff.management;
 
-public class Manager {
+import staff.Employee;
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, String NI, Double salary, String deptName) {
+        super(name, NI, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName(){
+        return this.deptName;
+    }
+
 }
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Hannah Green", "JN3456C", 10.00, "GitCommits");
+    }
+
+    @Test
+    public void canGetDeptName(){
+        assertEquals("GitCommits", manager.getDeptName());
+    }
+
+
 }
